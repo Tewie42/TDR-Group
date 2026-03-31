6b093b3b9302885dc49b57d8faf009b7c3288732
Tewie42 <tiaann@lowveldis.com>
2026-03-31 10:49:10 +0300

    Create project.js

---
+++ tools/scaffcalc/js/project.js
@@ -0,0 +1,13 @@
+// ------------------------------------------------------
+// S C A F F C A L C   P R O J E C T   S T A T E
+// ------------------------------------------------------
+
+const project = {
+    bays: [],
+    totalWidthM: 0,
+    totalHeightM: 0,
+    liftHeightM: 2,
+    platformLevels: 0,
+    gridSizeM: 1,
+    system: "Kwikstage",
+};
