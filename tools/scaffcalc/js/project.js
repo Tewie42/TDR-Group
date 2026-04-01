32258651e0dfa7e9ff3915759a49859947cc529d
Tewie42 <tiaann@lowveldis.com>
2026-04-01 10:39:41 +0300

    Update project.js

--- tools/scaffcalc/js/project.js
+++ tools/scaffcalc/js/project.js
@@ -1,16 +1,13 @@
-// ------------------------------------------------------
-// S C A F F C A L C   P R O J E C T   S T A T E
-// ------------------------------------------------------
-
 const project = {
     bays: [],
     totalWidthM: 10,
-    totalHeightM: 0,
+    totalHeightM: 10,   // <-- MUST NOT BE ZERO
     liftHeightM: 2,
     platformLevels: 3,
     gridSizeM: 1,
     system: "Kwikstage",
 };
+
 project.outriggers = {
     required: false,
     lengthM: 0
